4853c1e8ba21c6302464d5789043ed8d994b9f61
WoodyMiao <miao_lin@163.com>
2016-07-22 13:01:15 +0800

    edit make_dadi_snp_file.pl

--- make_dadi_snp_file.pl
+++ make_dadi_snp_file.pl
@@ -2,13 +2,13 @@
 use strict;
 use warnings;
 
-die "\nThis program makes DaDi SNP data file using gzipped fasta files. The ingroup reference is not considered.
-\nAuther: Woody\n
-Usage: $0 <list (column1: sample, column2: file, column3: \"Outgroup\"|PopID)> <output>\n\n" if @ARGV < 2;
+die "\nThis program makes DaDi SNP data file using gzipped fasta files.\n\nAuther: Woody\n
+Usage: $0 <list (column1: sampleID, column2: file, column3: \"Reference\"|\"Outgroup\"|PopID)> <output>\n\n" if @ARGV < 2;
 
 open LIST, "<", $ARGV[0];
 open O, ">", $ARGV[1];
 
+my $C1;
 my $C2;
 my @id;
 my @po;
@@ -17,9 +17,11 @@ my %id_pop;
 my %file;
 while (<LIST>) {
 	chomp;
-	my @a = split / /;
+	my @a = split /\s+/;
 	open $file{$a[0]}, "-|", "zcat $a[1]";
-	if ($a[2] eq "Outgroup") {
+	if ($a[2] eq "Reference") {
+		$C1 = $a[0];
+	} elsif ($a[2] eq "Outgroup") {
 		$C2 = $a[0];
 	} else {
 		push @id, $a[0];
@@ -28,19 +30,22 @@ while (<LIST>) {
 	}
 }
 close LIST;
-
 my %base_allele = ("A", ["A","A"], "C", ["C","C"], "G", ["G","G"], "T", ["T","T"], "R", ["A","G"], "Y", ["C","T"], "M", ["A","C"], "K", ["G","T"], "S", ["G","C"], "W", ["A","T"], "N", ["N","N"]);
 my %chr_gt;
-print O "Ref\t$C2\tAllele1\t", join("\t", sort keys %pop), "\tAllele2\t", join("\t", sort keys %pop), "\tChromosome\tOneBasedCoordinate\n";
-while (readline $file{$C2}) {
+print O "$C1\t$C2\tAllele1\t", join("\t", sort keys %pop), "\tAllele2\t", join("\t", sort keys %pop), "\tChromosome\tOneBasedCoordinate\n";
+while (readline $file{$C1}) {
 	chomp;
 	s/^>//;
 	last unless /^chr\w\d$/;
 	my $chr0 = $_;
-	my $seq0 = readline $file{$C2};
+	my $seq0 = readline $file{$C1};
 	chomp $seq0;
 	my $len = length $seq0;
 	my @seq;
+	push @seq, $seq0;
+	readline $file{$C2};
+	$seq0 = readline $file{$C2};
+	chomp $seq0;
 	push @seq, $seq0;
 	for (@id) {
 		my $chr1 = readline $file{$_};
@@ -50,17 +55,14 @@ while (readline $file{$C2}) {
 		push @seq, $seq1;
 	}
 	for my $coo (0 .. $len-1) {
-		my $out_allele = substr($seq[0], $coo, 1);
-		next unless $out_allele =~ /[ACGT]/;
 		my @base;
 		my %base;
-		for (1 .. @seq-1) {
+		for (2 .. @seq-1) {
 			my @b = @{$base_allele{substr($seq[$_], $coo, 1)}};
 			push @base, \@b;
 			++$base{$b[0]};
 			++$base{$b[1]};
 		}
-		next unless $base{$out_allele};
 		if ($base{N}) {
 			if ($base{N} > 22 or keys %base != 3) {
 				next;
@@ -84,7 +86,11 @@ while (readline $file{$C2}) {
 			push @num_allele0, $pop_allele{$_}{$allele0};
 			push @num_allele1, $pop_allele{$_}{$allele1};
 		}
-		print O "---\t", substr($seq[0], $coo-1, 3), "\t$allele0\t";
+		my $column1 = substr($seq[0], $coo-1, 3);
+		$column1 =~ s/N/-/g;
+		my $column2 = substr($seq[1], $coo-1, 3);
+		$column2 =~ s/N/-/g;
+		print O "$column1\t$column2\t$allele0\t";
 		print O join("\t", @num_allele0);
 		print O "\t$allele1\t";
 		print O join("\t", @num_allele1);
